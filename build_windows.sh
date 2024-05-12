d8c7fc8bfa7f1f7d0c654f28dba281cfc24a7fd1
matgd <28411337+matgd@users.noreply.github.com>
2024-05-12 12:02:46 +0200

    Windows build script fix

--- build_windows.sh
+++ build_windows.sh
@@ -1,5 +1,5 @@
 #!/bin/env bash
 
-go install github.com/fyne-io/fyne-cross@latest
-fyne-cross windows --pull
-fyne-cross windows -arch=amd64 -output bin/swrpg_calc_windows_amd64.exe ./src
+# go install github.com/fyne-io/fyne-cross@latest
+# fyne-cross windows --pull
+fyne-cross windows -arch=amd64 -output swrpg_calc_windows_amd64.exe --app-id matgd.github.com.starwarsrpg.dicehelper ./src
